f70fd1f33ff0362944a9cd5f5d2eeaf7abc1f0bc
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-05-07 16:58:10 -0700

    after adding variables in constructor

--- src/Oops.java
+++ src/Oops.java
@@ -1,13 +1,18 @@
 class Kunal{
     String name;
     int rollno;
+    Kunal(String name, int rollno){
+        this.name = name;
+        this.rollno = rollno;
+    }
     void print(){
         System.out.println("Your name is: "+ name + "\nYour Roll Number is: "+ rollno);
     }
 }
 public class Oops {
     public static void main(String[] args) {
-        Kunal kk = new Kunal();
+        Kunal kk = new Kunal("Kunal",21);
+
         kk.print();
 
     }
