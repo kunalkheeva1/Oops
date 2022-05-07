299554c5dfab9de736aeba7c7665bb29d4e9f48c
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-05-07 16:53:38 -0700

    Initial commit

---
+++ src/Oops.java
@@ -0,0 +1,16 @@
+class Kunal{
+    String name;
+    int rollno;
+    void print(){
+        System.out.println("Your name is: "+ name + "\nYour Roll Number is: "+ rollno);
+    }
+}
+
+
+public class Oops {
+    public static void main(String[] args) {
+        Kunal kk = new Kunal();
+        kk.print();
+
+    }
+}
