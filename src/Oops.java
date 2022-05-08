c156447c426c3a4aa6ba3be719cb92606bf3806f
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-05-07 17:41:07 -0700

    copy constructors.

--- src/Oops.java
+++ src/Oops.java
@@ -6,11 +6,17 @@ class Kunal{
         this.rollno = rollno;
         System.out.println("Your name is: "+ name + "\nYour Roll Number is: "+ rollno);
     }
+    Kunal(Kunal call){
+        System.out.println("When copy constructor is called!!");
+
+    }
 
 }
 public class Oops {
     public static void main(String[] args) {
-        Kunal kk = new Kunal("Kunal",21);
+    Kunal kk = new Kunal("Kunal",21);
+    Kunal nn= new Kunal(kk);
+
 
     }
 }
